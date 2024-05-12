cf85925b8b315b00f8e2e5efa0958b66a8e569b1
Khvoya <mokhkhvoya@gmail.com>
2024-05-12 12:07:10 +0300

    ci changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    headless: false,
+    headless: true,
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "https://sandbox-platform.faraway.com/",
 
